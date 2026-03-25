43c99dbb507784bc767aff122ef32b86c8159dca
Nkhata-4 <YanjaniNkhata08@outlook.com>
2026-03-25 10:57:11 +0000

    started portfolio

--- worksheet/portfolio/shapes.c
+++ worksheet/portfolio/shapes.c
@@ -6,9 +6,24 @@
 
 Point makePoint( float x, float y ) {
     Point new;
+    p1.x=2.3;
+    p1.y=1.2;
+
+    p2.x=2.3;
+    p2.y=3.4;
     // implementation
+    abs(lineLength(p1,p2))<1.0e-6
     return new;
 }
+
+Line makeLine( Point p1, Point p2 ) {
+    Line newLine;
+    distance = distance = sqrt((x2-x1)^2 + (y2-y1)^2)
+    newLine1=(p1, p2);
+    newLine2=(p2, p3);
+    return newLine;
+}
+
 
 // complete other functions below
 // - start with stubs as above
