4299c0f766bba355d261330eb50340cc2612aa86
Nkhata-4 <YanjaniNkhata08@outlook.com>
2026-03-25 14:16:43 +0000

    worked on portfolio

--- worksheet/portfolio/shapes.c
+++ worksheet/portfolio/shapes.c
@@ -6,25 +6,85 @@
 
 Point makePoint( float x, float y ) {
     Point new;
-    p1.x=2.3;
-    p1.y=1.2;
-
-    p2.x=2.3;
-    p2.y=3.4;
+    new.x=x;
+    new.y=y;
     // implementation
-    abs(lineLength(p1,p2))<1.0e-6
     return new;
 }
 
 Line makeLine( Point p1, Point p2 ) {
     Line newLine;
-    distance = distance = sqrt((x2-x1)^2 + (y2-y1)^2)
-    newLine1=(p1, p2);
-    newLine2=(p2, p3);
+    newLine.p1=(x, y);
+    newLine.p2=(x, y);
     return newLine;
 }
 
+Triangle makeTriangle( Point p1, Point p2, Point p3 ) {
+    Triangle newTriangle;
+    newTriangle.p=p1;
+    newTriangle.p=p2;
+    newTriangle.p=p3;
+    return newTriangle;
+}
 
+float lineLength( Line l) {
+    point1=l.p1;
+    point2=l.p2;
+    
+    int exp = 2; 
+    float sub_x = point2.x - point1.x;
+    float sub_y = point2.y - point1.y;
+    float lineLength = hypot(sub_x, sub_y);
+    return lineLength;
+}
+
+float triangleArea ( Triangle t) {
+    Point p1=t.p1;
+    Point p2=t.p2;
+    Point p3=t.p3;
+    float dist_1 = lineLength(p1, p2);
+    float dist_2 = lineLength(p2, p3);
+    float dist_3 = lineLength(p1, p3);
+    float semi_p = 0.5 * (dist_1 + dist_2 + dist_3)
+    float prod = (s-dist_1)(semi_p-dist_2)(semi_p-dist_3); 
+    float area = sqrt(semi_p * prod);
+    return area;
+}
+
+bool samePoint( Point p1, Point p2 ) {
+    if (abs(lineLength(p1,p2))<1.0e-6) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+bool pointInLine( Point p, Line l) {
+    p1=l.p1;
+    p2=l.p2;
+    if(abs(lineLength(p1,p))<1.0e-6) {
+        return true;
+    } else if (abs(lineLength(p,p2))<1.0e-6) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+bool pointInTriangle( Point p, Triangle t) {
+    p1=t.p1;
+    p2=t.p2;
+    p3=t.p3;
+    if(abs(lineLength(p1,p))<1.0e-6) {
+        return true;
+    } else if (abs(lineLength(p,p2))<1.0e-6) {
+        return true;
+    } else if (abs(lineLength(p,p3))<1.0e-6) {
+        return true;
+    } else {
+        return false;
+    }
+}
 // complete other functions below
 // - start with stubs as above
 // - compile regularly to test syntax
